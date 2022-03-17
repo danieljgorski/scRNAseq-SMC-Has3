f72907e37f90f181f3e37a461513bd5f3339a1cb
danieljgorski <danieljgorski@gmail.com>
2022-03-17 01:00:17 +0100

    added results directory creation

--- scripts/SMC-Has3-main-script.R
+++ scripts/SMC-Has3-main-script.R
@@ -1,4 +1,21 @@
+# make directories
+output_dirs <- c(
+    "results",
+    "results/objects",
+    "results/dimplots",
+    "results/cluster_markers")
+    
+for (i in output_dirs) {
+    if (!dir.exists(i)) {
+        dir.create(i)
+        print(paste0("made ", i, " directory"))
+        } else {
+            print(paste0(i, " directory already exists."))
+}
+}
+
 # run analysis in order
 source("scripts/SMC-Has3-clustering.R")
 source("scripts/SMC-Has3-annotation.R")
 source("scripts/SMC-Has3-dimplot.R")
+source("scripts/SMC-Has3-cluster-markers.R")
