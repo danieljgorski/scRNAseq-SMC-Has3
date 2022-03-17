0da65c1a6974b33a0b64b235f7b9e10f37fe37f0
danieljgorski <danieljgorski@gmail.com>
2022-03-17 17:08:29 +0100

    removed un-annotated cluster marker detection

--- scripts/SMC-Has3-annotation.R
+++ scripts/SMC-Has3-annotation.R
@@ -5,33 +5,6 @@ library(multtest)
 ## Load Seurat object----
 load("results/objects/obj.Rdata")
 
-## Find conserved cluster markers----
-Idents(obj) <- "seurat_clusters"
-DefaultAssay(obj) <- "RNA"
-idents_list <- levels(obj)
-conserved_markers_list <- list()
-for (i in idents_list) {
-  cluster <- i
-  conserved_markers <- FindConservedMarkers(obj,
-                                           ident.1 = cluster,
-                                           min.pct = 0.25,
-                                           logfc.threshold = 0.25,
-                                           only.pos = T,
-                                           assay = "RNA",
-                                           slot = "data",
-                                           grouping.var = "genotype")
-  conserved_markers <- conserved_markers[conserved_markers$WT_p_val_adj < 0.05, ]
-  conserved_markers <- conserved_markers[conserved_markers$KO_p_val_adj < 0.05, ]
-  conserved_markers$cluster <- i
-  conserved_markers$gene <- row.names(conserved_markers)
-  row.names(conserved_markers) <- NULL
-  conserved_markers_list[[i]] <- conserved_markers
-  remove(conserved_markers)
-}
-conserved_markers <- do.call(rbind, conserved_markers_list)
-remove(conserved_markers_list)
-write.csv(conserved_markers, file = "results/cluster_markers/conserved_markers.csv", row.names = F)
-
 ## Cluster annotation----
 DimPlot(obj, label = T)
 obj <- RenameIdents(obj,
